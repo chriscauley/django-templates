ce85e711b30b31dba859a39fa331b03879c3f8e1
chriscauley <chris@lablackey.com>
2014-04-30 17:46:58 -0400

    adding some minor tweaks

--- project/main/settings/felixmint.py
+++ project/main/settings/felixmint.py
@@ -1,14 +1,6 @@
 DEBUG = TEMPLATE_DEBUG = THUMBNAIL_DEBUG = True
 
-COMPRESS_ENABLED = True
-COMPRESS_OFFLINE = True
-
-DATABASES = {
-  'default': {
-    'ENGINE': 'django.db.backends.sqlite3',
-    'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-  }
-}
+COMPRESS_ENABLED = COMPRESS_OFFLINE = False
 
 SITE_URL = 'http://127.0.0.1:8000'
 
