cf901cbd398e78a022c2aa5a589dc927a7e45f38
chriscauley <chris@lablackey.com>
2014-09-03 19:32:38 -0400

    updating the way that settings are done

--- project/main/settings/_staging.py
+++
@@ -1,9 +0,0 @@
-#Sample settings for a staging server
-
-COMPRESS_ENABLED = True
-COMPRESS_OFFLINE = True
-
-STATIC_ROOT = os.path.join(BASE_DIR,'../shared/static')
-MEDIA_ROOT = os.path.join(BASE_DIR,'../shared/media')
-
-SITE_URL = "http://{{ projct_name }}.6ft.io"
