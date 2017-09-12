85fdae2cfa62f53ce99c68f0b2e1ef78101329e5
chriscauley <chris@lablackey.com>
2017-09-12 16:19:01 +0000

    updating startproject script to automate .e and .dev

--- startproject.sh
+++ startproject.sh
@@ -1,6 +1,12 @@
 if [ -n "$1" ]; then
     T="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/project"
     django-admin.py startproject $1 --template=$T --extension=sh --extension=conf $2
+    ln -s /home/chriscauley/.dev/ $1
+    cd $1
+    virtualenv .e
+    source .e/bin/activate
+    pip install -r requirements.txt
+    python manage.py migrate
     echo "Project $1 created"
     exit 0
 fi
