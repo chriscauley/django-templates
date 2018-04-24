37e260705771f55459e0857284b4a2f9ac16ffa7
chriscauley <chris@lablackey.com>
2018-04-24 00:40:49 +0000

    removing migrate and admin

--- project/main/urls.py
+++ project/main/urls.py
@@ -1,15 +1,11 @@
 from django.conf import settings
 from django.conf.urls import include, url
 
-from django.contrib import admin
 from django.contrib.auth import urls as auth_urls
 
 import lablackey.urls
 
-admin.autodiscover()
-
 urlpatterns = [
-  url(r'^admin/', include(admin.site.urls)),
   url(r'^auth/',include(auth_urls)),
   url(r'',include(lablackey.urls)),
 ]
--- startproject.sh
+++ startproject.sh
@@ -4,10 +4,12 @@ if [ -n "$1" ]; then
     ln -s /home/chriscauley/.dev/ $1
     ln -s /home/chriscauley/node_modules/ $1
     cd $1
+    echo "VENV time"
     virtualenv .e
     source .e/bin/activate
+    echo "requirements..."
     pip install -r requirements.txt
-    python manage.py migrate
+    #python manage.py migrate
     echo "Project $1 created"
     exit 0
 fi
